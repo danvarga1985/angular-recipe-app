85d0b04f75b5a38839df475d9c04d9b238eacbda
danvarga1985 <danvarga1985@gmail.com>
2020-09-21 15:10:06 +0200

    Adding Ingredient, Shopping list

---
+++ src/app/shared/dist/ingredient.model.js
@@ -0,0 +1,11 @@
+"use strict";
+exports.__esModule = true;
+exports.Ingredient = void 0;
+var Ingredient = /** @class */ (function () {
+    function Ingredient(name, amount) {
+        this.name = name;
+        this.amount = amount;
+    }
+    return Ingredient;
+}());
+exports.Ingredient = Ingredient;
