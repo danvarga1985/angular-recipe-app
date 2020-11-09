844ff8b9ddeac3dae52f202eafab956324ac980b
danvarga1985 <danvarga1985@gmail.com>
2020-11-09 07:40:45 +0100

    Adding modal alert to auth

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -18,6 +18,7 @@ import {HTTP_INTERCEPTORS, HttpClientModule} from '@angular/common/http';
 import { AuthComponent } from './auth/auth.component';
 import {LoadingSpinnerComponent} from './shared/loading-spinner/loading-spinner.component';
 import {AuthInterceptorService} from './services/auth-interceptor.service';
+import { AlertComponent } from './shared/alert/alert.component';
 
 
 @NgModule({
@@ -34,7 +35,8 @@ import {AuthInterceptorService} from './services/auth-interceptor.service';
     RecipeStartComponent,
     RecipeEditComponent,
     AuthComponent,
-    LoadingSpinnerComponent
+    LoadingSpinnerComponent,
+    AlertComponent
   ],
   imports: [
     BrowserModule,
--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -59,4 +59,8 @@ export class AuthComponent implements OnInit {
 
     form.reset();
   }
+
+  onHandleError(): void {
+    this.error = null;
+  }
 }
